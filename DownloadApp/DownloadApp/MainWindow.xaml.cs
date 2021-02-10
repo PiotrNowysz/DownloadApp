86f5de0c69f864b7e3539b977e3324c604c78e36
PiotrNowysz <pnowysz@gmail.com>
2021-02-10 19:56:46 +0100

    Button_Click function added

--- DownloadApp/DownloadApp/MainWindow.xaml.cs
+++ DownloadApp/DownloadApp/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DownloadApp
 {
@@ -23,6 +12,25 @@ namespace DownloadApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+           var currentUrl = WebsiteUrl.Text;
+
+            await Task.Run(() =>
+            {
+                var webClient = new WebClient();
+                var downloadedString = webClient.DownloadString(currentUrl);
+                
+
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    DisplayText.Text = "Done";
+                });
+                
+            }); 
         }
     }
 }
