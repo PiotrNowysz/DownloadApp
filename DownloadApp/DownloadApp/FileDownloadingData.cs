400cac4721a9ed4237fb04ff28080c166cfecbf6
PiotrNowysz <pnowysz@gmail.com>
2021-02-10 22:09:19 +0100

    summary added

--- DownloadApp/DownloadApp/FileDownloadingData.cs
+++ DownloadApp/DownloadApp/FileDownloadingData.cs
@@ -5,11 +5,20 @@ using System.Text;
 
 namespace DownloadApp
 {
+    /// <summary>
+    /// Class that contains user data
+    /// </summary>
     public class FileDownloadingData
     {
+        /// <summary>
+        /// The url to download website from
+        /// </summary>
         [Required]
         [StringLength(100, MinimumLength = 5)]
         public string Url { get; set; }
+        /// <summary>
+        /// The name of the file to save to
+        /// </summary>
         public string FileName { get; set; }
     }
 }
--- DownloadApp/DownloadApp/MainWindow.xaml.cs
+++ DownloadApp/DownloadApp/MainWindow.xaml.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Linq;
+
 
 namespace DownloadApp
 {
@@ -15,9 +15,21 @@ namespace DownloadApp
     public partial class MainWindow : Window
     {
         public string DownloadedString { get; set; }
+        /// <summary>
+        /// Object contains all the data form user. Needs to be validated in order to use it
+        /// </summary>
         public FileDownloadingData FileDownloadingData { get; set; } = new FileDownloadingData();
+        /// <summary>
+        /// Event that is fired when string from web has just been downloaded
+        /// </summary>
         public event Action<string> StringDownloaded = (x) => { };
+        /// <summary>
+        /// Event that is fired when file name was provided by the user
+        /// </summary>
         public event Action<string, string> FileNameProvided = (x, y) => { };
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
@@ -44,7 +56,11 @@ namespace DownloadApp
             FileName.Visibility = Visibility.Hidden;
 
         }
-
+        /// <summary>
+        /// Fired when user clicks submit button
+        /// </summary>
+        /// <param name="sender">Button</param>
+        /// <param name="e">Event args</param>
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
 
@@ -69,6 +85,9 @@ namespace DownloadApp
                 });
             }
         }
+        /// <summary>
+        /// Set controls state acording to download state
+        /// </summary>
         private void SetControlStateAfterDownload()
         {
             Application.Current.Dispatcher.Invoke(() =>
@@ -80,10 +99,19 @@ namespace DownloadApp
 
 
         }
+        /// <summary>
+        /// Saves downloaded website to file
+        /// </summary>
+        /// <param name="fileName">Name of the file to save to</param>
+        /// <param name="downloadedString">String with HTML code of the web</param>
         private void SaveToFile(string fileName, string downloadedString)
         {
             File.WriteAllText(fileName, downloadedString);
         }
+        /// <summary>
+        /// Validates data from user
+        /// </summary>
+        /// <returns>True if validation suceeded, otherwise false</returns>
         private bool Validate()
         {
             var validationContext = new ValidationContext(FileDownloadingData);
